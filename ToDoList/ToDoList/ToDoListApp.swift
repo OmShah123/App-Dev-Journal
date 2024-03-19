84e891d7bad15c96d65f1620d404c80cf2da96c4
OmShah123 <100872933+OmShah123@users.noreply.github.com>
2024-03-18 23:14:11 -0500

    Add files via upload

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Shah, Om on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
