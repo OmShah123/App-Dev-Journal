8d3d50d0ca35efb90c07b8b6285d8eada6797c8f
OmShah123 <100872933+OmShah123@users.noreply.github.com>
2024-04-24 01:34:04 -0500

    Add files via upload

---
+++ Pokedex/Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Om Shah on 4/01/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
